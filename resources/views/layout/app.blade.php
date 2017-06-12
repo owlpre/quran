2d47bb6e6a5b2f9f697fdfb8b903d2894a01aa9a
dwijpr <dwijpr@gmail.com>
2017-06-13 03:05:39 +0700

    quiz

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,7 +20,7 @@
     <body>
         @yield('content')
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
         {{ Html::script('bootstrap/3.3.7/dist/js/bootstrap.js') }}
+        @yield('script')
     </body>
 </html>
--- resources/views/quran/index.blade.php
+++ resources/views/quran/index.blade.php
@@ -8,8 +8,16 @@
                 '/font/qalammajeed.ttf'
             ) }});
         }
+        .form-control {
+            display: inline-block;
+            width: auto;
+        }
+        .sura-wrapper {
+            padding: 8px;
+        }
         .sura {
-            height: 150px;
+            padding: 8px;
+            border: 1px solid #cecece;
         }
     </style>
 @endsection
@@ -20,14 +28,28 @@
             <div class="col-sm-12 text-center">
                 {{ Html::image('/img/quranalkarim.png') }}
                 <br>
-                <span class="btn btn-info">
-                    <span class="badge">{{ count($suras) }}</span>
-                    Suras
-                </span>
-                <span class="btn btn-default">
-                    <span class="badge">{{ $aya_count }}</span>
-                    Ayas
-                </span>
+                <div class="action-hidden" style="display: none;">
+                    {{ Form::text('sura', '', [
+                        'class' => 'question form-control',
+                        'placeholder' => 'Number of Sura',
+                        'data-answer' => count($suras),
+                    ]) }}
+                    {{ Form::text('aya', '', [
+                        'class' => 'question form-control',
+                        'placeholder' => 'Number of Aya',
+                        'data-answer' => $aya_count,
+                    ]) }}
+                </div>
+                <div class="action-hidden">
+                    <span class="btn btn-info">
+                        <span class="badge">{{ count($suras) }}</span>
+                        Suras
+                    </span>
+                    <span class="btn btn-default">
+                        <span class="badge">{{ $aya_count }}</span>
+                        Ayas
+                    </span>
+                </div>
             </div>
         </div>
     </div>
@@ -35,14 +57,69 @@
     <div class="container">
         <div class="row">
                 @foreach ($suras as $sura)
-                    <div class="sura text-center col-sm-6 col-md-4 col-lg-3">
-                        {{ Html::image($sura->image()) }}
-                        <br>
-                        {{ $sura->name }} <span class="badge">{{ $sura->ayas()->count() }}</span>
-                        <br>
-                        {{ $sura->arti }}
+                    <div class="sura-wrapper text-center col-sm-6 col-md-4 col-lg-3">
+                        <div class="sura">
+                            {{ Html::image($sura->image()) }}
+                            <div class="action-hidden" style="display: none;">
+                                {{ Form::text('name', '', [
+                                    'class' => 'question form-control',
+                                    'placeholder' => 'Sura Name',
+                                    'data-answer' => $sura->name,
+                                ]) }}
+                                {{ Form::text('arti', '', [
+                                    'class' => 'question form-control',
+                                    'placeholder' => 'Sura Arti',
+                                    'data-answer' => $sura->arti,
+                                ]) }}
+                                {{ Form::text('number', '', [
+                                    'class' => 'question form-control',
+                                    'placeholder' => 'Number of Aya',
+                                    'data-answer' => $sura->ayas()->count(),
+                                ]) }}
+                            </div>
+                            <div class="action-hidden">
+                                <br>
+                                {{ $sura->name }} <span class="badge">{{ $sura->ayas()->count() }}</span>
+                                <br>
+                                {{ $sura->arti }}
+                            </div>
+                        </div>
                     </div>
                 @endforeach
         </div>
     </div>
+    <div id="main-menu" style="position: fixed;top: 0px;padding: 8px;">
+        <button class="btn btn-default">
+            <span class="glyphicon glyphicon-asterisk"></span>
+        </button>
+        <button class="btn btn-default">
+            <span class="glyphicon glyphicon-ok"></span>
+        </button>
+        <button class="btn btn-default">
+            <span class="glyphicon glyphicon-erase"></span>
+        </button>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        $(function () {
+            $("#main-menu .glyphicon.glyphicon-asterisk").click(function () {
+                $(".action-hidden").toggle();
+            });
+            $("#main-menu .glyphicon.glyphicon-ok").click(function () {
+                $(".question").each(function () {
+                    var el = $(this);
+                    var answer = el.val();
+                    var trueAnswer = el.data('answer');
+                    if (answer != trueAnswer) {
+                        el.val('');
+                    }
+                });
+            });
+            $("#main-menu .glyphicon.glyphicon-erase").click(function () {
+                $(".question").val('');
+            });
+        });
+    </script>
 @endsection
